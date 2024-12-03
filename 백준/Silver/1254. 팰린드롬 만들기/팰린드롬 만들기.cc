10f4a4ee30dd496e8c7d9bf542e027c180de7e6a
bootkorea <68296100+bootkorea@users.noreply.github.com>
2024-12-03 19:22:05 +0900

    [Silver II] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1254. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string str;
+int result = 0;
+bool flag = 0;
+
+bool Solve(string s)
+{
+    int len = s.length() / 2;
+
+    for (int i = 0; i < len; i++)
+    {
+        if (s[i] != s[s.length() - i - 1])
+            return 0;
+    }
+    return 1;
+}
+
+int main()
+{
+    cin >> str;
+    result = str.length() * 2 - 1;
+    for (int i = 0; i < str.length(); i++)
+    {
+        string temp = str.substr(i, str.length() - i);
+
+        if (Solve(temp))
+        {
+            result = str.length() + i;
+            break;
+        }
+    }
+
+    cout << result;
+
+    return 0;
+}
